93cd64596e610a57cebc0f0909de65ed3ba4144a
Fabiane miranda <fabianemiranda936@gmail.com>
2021-06-17 15:55:35 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Button,Input, Text } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import styles from './style/Mainstyle';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Login from './screens/Login';
+import Principal from './screens/Principal';
+import Cadastro from './screens/Cadastro';
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Principal" component={Principal} />
+      <Stack.Screen name="Cadastro" component={Cadastro} />
+    </Stack.Navigator>
+  );
+}
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
+
+
+
+
